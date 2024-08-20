6d480c6858f290b701fb238bb0bad222736746b0
ChengKunLiu <614368586@qq.com>
2024-08-20 10:34:10 +0800

    添加SplashScreen启动屏幕

--- app/src/main/java/com/laomuji666/compose/quickly/MainActivity.kt
+++ app/src/main/java/com/laomuji666/compose/quickly/MainActivity.kt
@@ -10,23 +10,46 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.laomuji666.compose.quickly.ui.theme.QuicklyTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val installSplashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        var uiState:MainUiState by mutableStateOf(MainUiState.Loading)
+        installSplashScreen.setKeepOnScreenCondition {
+            uiState == MainUiState.Loading
+        }
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .collect{
+                        uiState = it
+                    }
+            }
+        }
+
         setContent {
             QuicklyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = viewModel.text,
+                        name = "Compose",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/laomuji666/compose/quickly/MainViewModel.kt
+++ app/src/main/java/com/laomuji666/compose/quickly/MainViewModel.kt
@@ -1,10 +1,30 @@
 package com.laomuji666.compose.quickly
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
-    val text:String = "Hilt"
+    private val _mainUiState = MutableStateFlow<MainUiState>(MainUiState.Loading)
+    val uiState = _mainUiState.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000),MainUiState.Loading)
+
+    init {
+        viewModelScope.launch {
+            //这里可以是向服务器请求数据
+            delay(1000)
+            _mainUiState.value = MainUiState.Success
+        }
+    }
+}
+
+sealed interface MainUiState{
+    data object Loading:MainUiState
+    data object Success:MainUiState
 }
