04f6c4d25272e802dbf56fef3163882dbae654f6
ChengKunLiu <614368586@qq.com>
2024-09-09 14:57:43 +0800

    引入firebase的database和storage,存储联系人信息.

--- core-logic/src/main/kotlin/com/laomuji666/compose/core/logic/di/RepositoryModule.kt
+++ core-logic/src/main/kotlin/com/laomuji666/compose/core/logic/di/RepositoryModule.kt
@@ -1,13 +1,11 @@
 package com.laomuji666.compose.core.logic.di
 
-import android.content.Context
 import com.laomuji666.compose.core.logic.repository.contacts.ContactsRepository
 import com.laomuji666.compose.core.logic.repository.http.HttpRepository
 import com.laomuji666.compose.core.logic.repository.http.HttpService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -24,9 +22,7 @@ class RepositoryModule {
 
     @Singleton
     @Provides
-    fun bindContactsRepository(
-        @ApplicationContext context: Context
-    ): ContactsRepository {
-        return ContactsRepository(context)
+    fun bindContactsRepository(): ContactsRepository {
+        return ContactsRepository()
     }
 }
