3baf30680d674f76f050f7b9271ec60ba43d9efe
ChengKunLiu <614368586@qq.com>
2024-12-12 10:12:43 +0800

    Update GEMINI_API_KEY

--- core-logic/common/build.gradle.kts
+++ core-logic/common/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     buildTypes {
         debug {
             buildConfigField("String","WEB_CLIENT_ID","\"905105202439-5v3lh96r8pgo1ubuv2qme1h6vahn4imi.apps.googleusercontent.com\"")
-            buildConfigField("String","GEMINI_API_KEY","\"AIzaSyAFmmPGw8rRLBiMmIt7XwU-P8cSVuRyKrY\"")
+            buildConfigField("String","GEMINI_API_KEY","\"AIzaSyAwC28Brrujfj43BOt6QKhp2U4bR5PbLgI\"")
         }
         release {
             buildConfigField("String","WEB_CLIENT_ID","\"905105202439-5v3lh96r8pgo1ubuv2qme1h6vahn4imi.apps.googleusercontent.com\"")
-            buildConfigField("String","GEMINI_API_KEY","\"AIzaSyAFmmPGw8rRLBiMmIt7XwU-P8cSVuRyKrY\"")
+            buildConfigField("String","GEMINI_API_KEY","\"AIzaSyAwC28Brrujfj43BOt6QKhp2U4bR5PbLgI\"")
         }
     }
 }
